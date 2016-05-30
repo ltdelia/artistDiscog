414afe189503c3f2f1eae9e4573879a35641c625
ltdelia <delia.louis@gmail.com>
2016-05-30 19:11:31 -0400

    added view comments button to see comments previously posted

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -1,11 +1,12 @@
 $.getJSON('/albums', function(data) {
   for (var i = 0; i<data.length; i++){
-    $('#albums').append('<tr><td>'+ data[i].album + '</td><td>'+ data[i].date + '</td><td>'+ data[i].label + '</td><td>'+ data[i].format + '</td><td><button data-id="' + data[i]._id + '" data-toggle="modal" href="#modal-id" id="note" class="btn btn-default">Comment</button></td></tr>');
+    $('#albums').append('<tr><td>'+ data[i].album + '</td><td>'+ data[i].date + '</td><td>'+ data[i].label + '</td><td>'+ data[i].format + '</td><td><button data-id="' + data[i]._id + '" data-toggle="modal" href="#viewcomments" id="see" class="btn btn-primary">View Comments</button></td><td><button data-id="' + data[i]._id + '" data-toggle="modal" href="#modal-id" id="note" class="btn btn-default">Comment</button></td></tr>');
   }
 });
 
-$(document).on('click', 'button', function(){
-  // $('#noteModal').empty();
+// Click Event to Create Comments
+$(document).on('click', '#note', function(){
+  // Grab the data-id associated with this "Comment" button
   var thisId = $(this).attr('data-id');
 
   $.ajax({
@@ -14,22 +15,22 @@ $(document).on('click', 'button', function(){
   })
     .done(function( data ) {
       console.log(data);
+      // Inject the data related to the data-id into the html
       $('#album').html(data.album);
       $('#date').html(data.date);
       $('#label').html(data.label);
       $('#format').html(data.format);
-      
-      $('#modal-footer').append('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>');
-      $('#modal-footer').append('<button class="btn btn-primary" data-id="' + data._id + '" id="savenote">Save Note</button>');
+      $('#savenote').attr('data-id', data._id);
 
 
       if(data.note){
-        $('#titleinput').val(data.note.title);
-        $('#bodyinput').val(data.note.body);
+        $('#comment-title').val(data.note.title);
+        $('#comment-body').val(data.note.body);
       }
     });
 });
 
+// Click Event to Save Comments
 $(document).on('click', '#savenote', function(){
   var thisId = $(this).attr('data-id');
 
@@ -37,20 +38,36 @@ $(document).on('click', '#savenote', function(){
     method: "POST",
     url: "/albums/" + thisId,
     data: {
-      title: $('#titleinput').val(),
-      body: $('#bodyinput').val()
+      title: $('#comment-title').val(),
+      body: $('#comment-body').val()
     }
   })
     .done(function( data ) {
       console.log(data);
-      $('#modal-id').empty();
     });
 
 
-  $('#titleinput').val("");
-  $('#bodyinput').val("");
+  $('#comment-title').val("");
+  $('#comment-body').val("");
 });
 
+
+// Click Event to View Comments Previously Posted
+$(document).on('click', '#see', function(){
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "GET",
+    url: "/albums/" + thisId,
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#title').html(data.album);
+      $('#comment-header').html(data.note.title);
+      $('#comment-text').html(data.note.body);
+
+    });
+});
 
 
 
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
+// Dependencies
+// Creating an instance of Express
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+// Assigning a PORT. If no PORT is specified, default to 8080.
 var PORT = process.env.PORT || 8080;
 
+// Using morgan for Node -- logging the GET/POST requests in our app
 app.use(logger('dev'));
+
+// Using body-parser
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+// Assigning a route for static content; makes our public folder "visible"
 app.use(express.static(__dirname + '/public'));
 
 var staticContentFolder;
 staticContentFolder = __dirname + '/app/public';
-
 
 app.use('/static', express.static(staticContentFolder));
 
@@ -28,8 +35,10 @@ db.once('open', function() {
   console.log('Mongoose connection successful.');
 });
 
+// Routes
 require("./app/controllers/controller.js")(app);
 
+// Listening for the PORT
 app.listen(PORT, function() {
   console.log('App running on port ' + PORT + ' !');
 });
